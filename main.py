0ec4ab6e562d8b50818d4d5120dd204a9f08c2fd
CoolFool <devrocks149@gmail.com>
2020-07-25 21:23:22 +0530

    play command update

--- main.py
+++ main.py
@@ -42,17 +42,6 @@ async def stop_server(ctx):
 
 
 @client.command()
-async def get_ip(ctx):
-    status = csgo.get_server_status()
-
-    if (status == "stopped" or status == "stopping"):
-        await ctx.send(f"CS:GO Server - Server is NOT ACTIVE")
-    else:
-        ip = csgo.get_instance_IP()
-        await ctx.send(f"CS:GO Server - IP is `{ip}`")
-
-
-@client.command()
 async def status(ctx):
     status = csgo.get_server_status()
     await ctx.send(f"CS:GO Server Status - `{status}`")
@@ -61,19 +50,23 @@ async def status(ctx):
 @client.command()
 async def help(ctx):
     response_title = "Command List"
-    command_list = "`ping`: Returns bot's latency\n`start_server`: Starts csgo server\n`stop_server`: Stops csgo server\n`get_ip`: Returns server's IP\n`status`: Returns server's running status"
+    command_list = "`ping`: Returns bot's latency\n`start_server`: Starts csgo server\n`stop_server`: Stops csgo server\n`play`: Connect to server\n`status`: Returns server's running status"
     emb = discord.Embed(title=response_title, description=command_list, color=0x679eff)
     await ctx.send(embed=emb)
 
 
 @client.command()
 async def play(ctx):
-    response_title = "Connect to Server"
-    ip = csgo.get_instance_IP()
-    description = "`Direct Connect`: steam://connect/{0}:27015/Urban_Hunger1\n`Console CMD`: `connect {0}:27015;password Urban_Hunger1`".format(ip)
-    emb = discord.Embed(title=response_title, description=description).set_footer(
-        text="glhf!")
-    await ctx.send(embed=emb)
+    status = csgo.get_server_status()
+
+    if status == "stopped" or status == "stopping":
+        await ctx.send(f"CS:GO Server - Server is NOT ACTIVE")
+    else:
+        ip = csgo.get_instance_IP()
+        response_title = "Connect to Server"
+        description = "`Direct Connect`: steam://connect/{0}:27015/Urban_Hunger1\n`Console CMD`: `connect {0}:27015;password Urban_Hunger1`".format(ip)
+        emb = discord.Embed(title=response_title, description=description).set_footer(text="glhf!")
+        await ctx.send(embed=emb)
 
 
 if __name__ == "__main__":
